60924ae09888dbdbcd87235876f842ccfd9882c1
Moathe Alghazali <145180153+moathex@users.noreply.github.com>
2023-09-19 22:39:26 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://hello-world-spring-glitter-76a7.v685176.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNDkwOTMwNmEyYWEyY2UzMjVmMjc2MWJlYzVjYmEzNiIsInN1YiI6IjY1MGE1M2MyY2FkYjZiMDBhYmM2NTBkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pYBFffdN8IkRiY4JHaqdx9UIjEn25bkMLoFUEnoGSbc"
+};
